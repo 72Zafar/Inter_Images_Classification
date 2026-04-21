cc954f15c7c6d4f0e301ebce6b618d3dfd3daada
72Zafar <zafaralikhanfs45@gmail.com>
2026-04-22 02:39:43 +0500

    Folder structure is done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Inter_Images_Classification",
+    version="0.1",
+    author="Zafar Ali",
+    author_email= "zafarali@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
